fbe3315d70f175c1aa21a206429cf0e06b961906
ryeetc <rclancaster1@gmail.com>
2022-11-24 16:11:01 -0500

    add quote api request to use effect and handleclick

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,22 +6,47 @@ import { useState, useEffect } from "react";
 const Home = () => {
 
     const [cat, setCat] = useState([])
+    const [quote, setQuote] = useState([])
+    const [attribution, setAttribution] = useState([])
+
+
 
     useEffect(() => {
+        let random = Math.floor(Math.random() * 1000) + 1
+        
         const catApi = axios.get("https://api.thecatapi.com/v1/images/search")
         catApi  
             .then((response) => {
                 let pic = response.data[0].url
                 setCat(pic)
             })
+
+        const quoteApi = axios.get("https://type.fit/api/quotes")
+            quoteApi
+                .then((response) => {
+                    let quote = response.data[random]
+                    setQuote(quote.text)
+                    setAttribution(quote.author)
+                })
     }, [])
 
     const handleClick = () => {
+
+        let random = Math.floor(Math.random() * 1000) + 1
+        
         const catApi = axios.get("https://api.thecatapi.com/v1/images/search")
         catApi  
             .then((response) => {
                 let pic = response.data[0].url
                 setCat(pic)
+            })
+
+        const quoteApi = axios.get("https://type.fit/api/quotes")
+        quoteApi
+            .then((response) => {
+                let quote = response.data[random]
+                setQuote(quote.text)
+                setAttribution(quote.author)
             })
         
     }
@@ -32,7 +57,7 @@ const Home = () => {
                 <h1 className="main__title">Click the button below to generate a motivational poster</h1>
                 <div className="main__container">
                     <img className="main__container--img" src={cat} alt="random cat"></img>
-                    <h3 className="main__container--text">Text will go here</h3>
+                    <h3 className="main__container--text">{quote}</h3>
                 </div>
                 <button onClick={handleClick} className="main__button">Generate</button>
             </main>
